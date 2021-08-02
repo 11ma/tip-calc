d7f618b476c5e128ead8f32ef0809056e365acb1
Michael A <mikeade08l@GMAIL.COM>
2021-08-03 00:11:54 +0100

    image tag now has alt

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import { Title, InputContainer, InputError } from "../modules/Bill.module.scss";
 
-const Bill = ({ value, onChange, src, validate }) => (
+const Bill = ({ value, onChange, src, validate, alt = "dollar icon" }) => (
   <div>
     <div className={Title}>
       <h3>bill</h3>
@@ -12,7 +12,7 @@ const Bill = ({ value, onChange, src, validate }) => (
         [InputError]: validate === true,
       })}
     >
-      <img src={src} />
+      <img src={src} alt={alt} />
       <input value={value} onChange={onChange} required />
     </div>
   </div>

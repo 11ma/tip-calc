6de783aaf4df16dd52c73794fae2a16c47f48adc
Michael A <mikeade08l@GMAIL.COM>
2021-08-02 23:06:43 +0100

    made style responsive for mobile

--- src/components/ValuePerPerson.js
+++ src/components/ValuePerPerson.js
@@ -4,14 +4,14 @@ const ValuePerPerson = ({ tipPerPerson, totalPerPerson }) => (
   <>
     <div className={styles.ValuePerPersonLayout}>
       <div>
-        <h2 className={styles.TipAmountTotal}>Tip Amount </h2>
+        <h2 className={styles.TipAmountTotal}>tip amount </h2>
         <h3>/ person</h3>
       </div>
       <h2 className={styles.figures}>£{tipPerPerson.toFixed(2)}</h2>
     </div>
     <div className={styles.ValuePerPersonLayout}>
       <div>
-        <h2 className={styles.TipAmountTotal}>Total </h2>
+        <h2 className={styles.TipAmountTotal}>total </h2>
         <h3>/ person</h3>
       </div>
       <h2 className={styles.figures}>£{totalPerPerson.toFixed(2)}</h2>
